0d0065a57a774b85ffc0082600d0dbd4e0a4ca54
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-03 19:58:01 +0200

    ta-boston clasa

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       hello boston <i className="fa-solid fa-house"></i>
+      <img src="/images/logo.jpg" alt="alt"></img>
     </div>
   );
 }

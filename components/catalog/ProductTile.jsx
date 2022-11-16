4150e04c30949ff99b5809e148c3c7d22da7f5dd
Oancea Alin <74875548+Oanee@users.noreply.github.com>
2022-11-16 13:30:43 +0200

    clasa + tema

---
+++ components/catalog/ProductTile.jsx
@@ -0,0 +1,38 @@
+export default function ProductTile() {
+  return (
+    <>
+      <article className="product-tile">
+        <header className="product-tile-header">
+          <div className="product-tile-image">
+            <picture>
+              <img src="/images/products/product-01a.jpg" alt="Suit"></img>
+            </picture>
+
+            <span className="badge">%</span>
+          </div>
+
+          <h1 className="product-tile-title">
+            <span>Thread Affair </span>
+            <span>Traveller </span>
+            Suit
+          </h1>
+          <h2 className="product-tile-subtitle">London fit Semi-slim</h2>
+        </header>
+
+        <section className="product-tile-pricing">
+          <span className="product-tile-regular">499.00</span>
+
+          <span className="product-tile-price">
+            USD <span>399.00</span>
+          </span>
+        </section>
+
+        <footer className="product-tile-footer">
+          <button className="button" title="Add to Cart" type="button">
+            Add to Cart
+          </button>
+        </footer>
+      </article>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import Footer from '../components/common/footer/Footer';
 import Header from '../components/common/header/Header';
+import HomeCatalogPreview from '../components/homepage/HomeCatalogPreview';
+import HomeNewsletter from '../components/homepage/HomeNewsletter';
 import HomePayment from '../components/homepage/HomePayment';
+import HomeSocial from '../components/homepage/HomeSocial';
 
 export default function Home() {
   return (
@@ -10,8 +13,20 @@ export default function Home() {
       </header>
 
       <main className="content">
-        <div className="content-main container">
+        <div className="content-main">
+          <HomeCatalogPreview></HomeCatalogPreview>
+
+          <HomeCatalogPreview></HomeCatalogPreview>
+
+          <HomeCatalogPreview></HomeCatalogPreview>
+
+          <HomeCatalogPreview></HomeCatalogPreview>
+
+          <HomeSocial></HomeSocial>
+
           <HomePayment></HomePayment>
+
+          <HomeNewsletter></HomeNewsletter>
         </div>
       </main>
 
